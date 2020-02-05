dd62580e02f270f454ed024a76201af038409303
Deiby López <deibyalopez@gmail.com>
2020-02-04 19:43:09 -0500

    ejercicios en javascript1

---
+++ Ejercicio 7/index.js
@@ -0,0 +1,42 @@
+
+// function suma(añoNacimiento, edad) {
+//     var resultado = añoNacimiento + edad 
+//     return resultado} 
+
+// console.log (suma(1996, 24))
+
+
+// var edad = 15
+// var edadPermitida = 18
+
+// function compararEdad (edad, edadPermitida){
+//     if  (edad >= edadPermitida) {
+
+// return "mayor de edad" } 
+
+// else {return "menor de edad"} 
+// }
+// console.log(compararEdad(edad,edadPermitida))
+
+// function evaluate(x) {
+//     if (x > 0) { return "Postivo" }
+//     else if (x < 0) { return "Negativo" }
+//     else { return "Cero" }
+// }
+// console.log(evaluate(-2))
+
+// function letter(letra) {
+//     if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u") { return "Vocal" }
+//     else if (letra >= 0 || letra <= 0 || letra == "." ) {return "Debes poner una letra"}
+//     else { return "Consonante"}
+// }
+// console.log(letter(2))
+
+
+function multiple(x) {
+    if (x % 5 === 0 || x % 11 === 0) { return true }
+    else {return false}
+}
+
+console.log (multiple (16))
+
