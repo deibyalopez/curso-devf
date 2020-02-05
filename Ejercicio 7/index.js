aee38825fc8328849120e9e8cb6eb17b27eaf6d2
Deiby López <deibyalopez@gmail.com>
2020-02-04 21:46:33 -0500

    objetos en JavaScript

--- Ejercicio 7/index.js
+++ Ejercicio 7/index.js
@@ -33,10 +33,39 @@
 // console.log(letter(2))
 
 
-function multiple(x) {
-    if (x % 5 === 0 || x % 11 === 0) { return true }
-    else {return false}
+// function multiple(x) {
+//     if (x % 5 === 0 || x % 11 === 0) { return true }
+//     else {return false}
+// }
+
+// console.log (multiple (16))
+
+var myPenguin = {
+    character: "Masha",
+    origin: ["Death and the Penguin", "Penguin Lost"],
+    autor: "Andrey Kurkov"
 }
 
-console.log (multiple (16))
+console.log("Hola mi nombre es", myPenguin.character, ". Mi autor,", myPenguin.autor, ", me dió vida en", myPenguin.origin[0], "y en", myPenguin.origin[1])
 
+myPenguin.puedeVolar = false
+myPenguin.graznar = function () { return "kaww kaww" }
+
+
+myPenguin.saludar = function() { 
+    return "Hola mi nombre es " + this.character + ". Mi autor," + this.autor + ", me dió vida en" + this.origin[0] + "y en" + this.origin
+}
+
+console.log (saludar())
+
+myPenguin.character = "Señor Pingu"
+
+console.log(myPenguin.character)
+
+
+myPenguin.volar = function() {
+    if (this.puedeVolar == true) {return "¡Puedo Volar!"}
+    else {return "¡No puedo volar!"}
+}
+
+console.log(myPenguin.volar())
